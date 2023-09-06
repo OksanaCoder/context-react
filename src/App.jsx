e7fc1ff74cfdcaebb0a2c6c8f57de5bd312c7500
Olena Zdanovska <freshjs@gmail.com>
2023-09-06 11:11:04 +0300

    Example use children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
-import CiaoSection from './components/CiaoSection';
+
+const List = ({title, children}) => {
+  return (
+    <article>
+      <h3 title={title}>List {title}</h3>
+      <ul>{children}</ul>
+    </article>
+  );
+};
 
 function App() {
   return (
     <>
-      <CiaoSection />
+      <List title="first">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </List>
+      <List title="second">
+        <li>10</li>
+        <li>20</li>
+      </List>
     </>
   );
 }
