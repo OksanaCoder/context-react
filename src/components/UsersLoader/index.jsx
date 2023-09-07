7c0e4fbe82dea808107cc3f8ebfac80149e31e0d
Olena Zdanovska <freshjs@gmail.com>
2023-09-07 11:01:23 +0300

    Ready pagination

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import styles from './UsersLoader.module.scss';
 
 class UsersLoader extends Component {
   constructor(props) {
@@ -7,12 +8,16 @@ class UsersLoader extends Component {
       users: [],
       error: null,
       isFetching: false,
+      currentPage: 1,
     };
   }
 
-  componentDidMount() {
+  load = () => {
+    const { currentPage } = this.state;
     this.setState({ isFetching: true });
-    fetch('https://randomuser.me/api/?results=5&seed=fm20231')
+    fetch(
+      `https://randomuser.me/api/?results=5&seed=fm20231&page=${currentPage}`
+    )
       .then((response) => response.json())
       .then((data) => {
         this.setState({ users: data.results });
@@ -24,10 +29,28 @@ class UsersLoader extends Component {
       .finally(() => {
         this.setState({ isFetching: false });
       });
+  };
+
+  componentDidMount() {
+    this.load();
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.currentPage !== prevState.currentPage) {
+      this.load();
+    }
   }
 
+  prevPage = () => {
+    if (this.state.currentPage > 1) {
+      this.setState((state, props) => ({ currentPage: state.currentPage - 1 }));
+    }
+  };
+
+  nextPage = () =>
+    this.setState((state, props) => ({ currentPage: state.currentPage + 1 }));
+
   render() {
-    const { isFetching, error, users } = this.state;
+    const { isFetching, error, users, currentPage } = this.state;
     if (isFetching) {
       return <h2>Loading...</h2>;
     }
@@ -37,6 +60,13 @@ class UsersLoader extends Component {
     return (
       <section>
         <h2>Users:</h2>
+        <div>
+          <button onClick={this.prevPage} disabled={currentPage === 1}>
+            &lt; prev
+          </button>
+          <span>&nbsp;{currentPage}&nbsp;</span>
+          <button onClick={this.nextPage}>next &gt;</button>
+        </div>
         <ul>
           {users.map((user) => (
             <li key={user.login.uuid}>{user.email}</li>
