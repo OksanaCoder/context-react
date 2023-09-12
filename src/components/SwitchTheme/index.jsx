3f6644061dc68e1bdee33d5c37a2d4bc1ab6ff7c
Olena Zdanovska <freshjs@gmail.com>
2023-09-12 11:46:57 +0300

    Add '@mui/icons-material'

--- src/components/SwitchTheme/index.jsx
+++ src/components/SwitchTheme/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import LightModeIcon from '@mui/icons-material/LightMode';
-// import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { LightMode, DarkMode } from '@mui/icons-material';
 import { ThemeContext } from '../../contexts';
 import { THEMES } from '../../constants';
