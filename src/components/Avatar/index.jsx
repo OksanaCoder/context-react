4f3141362df5df9e30b4b707991e658ac12f159a
Olena Zdanovska <freshjs@gmail.com>
2023-09-12 10:43:59 +0300

    Example contextType

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { UserContext } from '../../contexts';
 
-const Avatar = () => {
-  const render = ({ user: { avatar } }) => {
+class Avatar extends Component {
+  render() {
+    const {
+      user: { avatar },
+    } = this.context;
     return (
       <div>
         <img src={avatar} alt="avatar" />
       </div>
     );
-  };
+  }
+}
 
-  return <UserContext.Consumer>{render}</UserContext.Consumer>;
-};
+Avatar.contextType = UserContext;
+
+// const Avatar = () => {
+//   const render = ({ user: { avatar } }) => {
+//     return (
+//       <div>
+//         <img src={avatar} alt="avatar" />
+//       </div>
+//     );
+//   };
+
+//   return <UserContext.Consumer>{render}</UserContext.Consumer>;
+// };
 
 export default Avatar;
