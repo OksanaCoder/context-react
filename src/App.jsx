7788d4d614626dce406af4782a78f09bf8d4da44
Olena Zdanovska <freshjs@gmail.com>
2023-09-12 10:10:13 +0300

    Start work with Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { UserContext } from './contexts';
-import Tree from './components/Tree';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import UsersPage from './pages/UsersPage';
+import Page404 from './pages/Page404';
+import LoaderPage from './pages/LoaderPage';
+import UsersBlock from './pages/LoaderPage/UsersBlock';
+import EventsBlock from './pages/LoaderPage/EventsBlock';
+import ProductsBlock from './pages/LoaderPage/ProductsBlock';
 
 class App extends Component {
   constructor(props) {
@@ -10,18 +19,37 @@ class App extends Component {
         id: 1,
         firstName: 'Brad',
         lastName: 'Pitt',
-        isSelect: false
+        isSelect: false,
       },
     };
   }
 
-  selectorUser = (id) => {this.setState({user:{...this.state.user, isSelect: !this.state.user.isSelect}})}
+  selectorUser = (id) => {
+    this.setState({
+      user: { ...this.state.user, isSelect: !this.state.user.isSelect },
+    });
+  };
 
   render() {
     const { user } = this.state;
     return (
       <UserContext.Provider value={{ user, selectorUser: this.selectorUser }}>
-        <Tree />
+        <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/loader/" element={<LoaderPage />}>
+              <Route path="users" element={<UsersBlock />} />
+              <Route path="events" element={<EventsBlock />} />
+              <Route path="products" element={<ProductsBlock />} />
+            </Route>
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </main>
+        <Footer />
+        </BrowserRouter>
       </UserContext.Provider>
     );
   }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CardUser from '../../components/CardSection/CardUser';
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <CardUser />
     </div>
   );
 };
