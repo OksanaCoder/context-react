f57b9cdaf8006af28444f8f71cbaab708447029d
Olena Zdanovska <freshjs@gmail.com>
2023-09-12 11:46:35 +0300

    Add '@mui/icons-material'

--- src/components/SwitchTheme/index.jsx
+++ src/components/SwitchTheme/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+// import LightModeIcon from '@mui/icons-material/LightMode';
+// import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { LightMode, DarkMode } from '@mui/icons-material';
 import { ThemeContext } from '../../contexts';
+import { THEMES } from '../../constants';
 
 const SwitchTheme = () => {
   const render = ([theme, changeTheme]) => {
-    return <button onClick={changeTheme}>switch Theme</button>;
+    return (
+      <div onClick={changeTheme}>
+        {theme === THEMES.LIGHT ? <DarkMode /> : <LightMode />}
+      </div>
+    );
   };
   return <ThemeContext.Consumer>{render}</ThemeContext.Consumer>;
 };
